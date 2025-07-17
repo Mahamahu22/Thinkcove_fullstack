406bfbd75628a24f50ff14eb6a577c564339f28d
Mahamahu22 <mahalakshmirajan07@gmail.com>
2025-07-17 17:00:34 +0530

    updated

---
+++ DAY1/let-example.js
@@ -0,0 +1,6 @@
+let a = 1, b = 2;
+if (true) {
+  let a = 3;  
+  b = 4;      
+}
+console.log(a, b)
---
+++ DAY1/var-example.js
@@ -0,0 +1,6 @@
+var a = 1, b = 2;
+if (true) {
+  var a = 3;
+  b = 4;      
+}
+console.log(a, b);
